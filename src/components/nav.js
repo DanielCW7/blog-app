ac67a61422bbb3c3fc14196d1c0c1b34d57ca53c
daniel <dev.danielcw@gmail.com>
2023-11-05 16:35:50 -0600

    copy edit

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,8 +5,8 @@ const Nav = () => {
         <div className="max-w-7xl m-auto pl-10 pr-10 pb-2 pt-2">
             <div className="text-right">
                 <span className="p-2"> Home </span>
-                <span className="p-2"> alt1 </span>
-                <span className="p-2"> alt2 </span>
+                <span className="p-2"> about </span>
+                <span className="p-2"> Contact </span>
             </div>
         </div>
     )
