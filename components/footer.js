fad031d21fb6f20d2976625af2abf8d32ad416fb
daniel <dev.danielcw@gmail.com>
2023-11-29 14:19:29 -0600

    added links to footer

--- components/footer.js
+++ components/footer.js
@@ -4,16 +4,17 @@ const Footer = () => {
     return (
         <footer className="max-w-7xl m-auto py-10 px-10 grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             <div className="flex flex-col gap-5">
-                <Link href="/"> home </Link>
-                <Link href="/about"> about </Link>
-                <Link href="/contact"> contact </Link>                
+                <Link href="/"> Home </Link>
+                <Link href="/about"> About </Link>
+                <Link href="/contact"> Contact </Link>        
+                <Link href="/admin"> Admin </Link>                        
             </div>
             <div className="flex flex-col gap-5">
-                <Link href="/about"> Github </Link>
-                <Link href="/contact"> Linkedin </Link>                
+                <Link href="https://github.com/DanielCW7" target="_blank"> Github </Link>
+                <Link href="https://www.linkedin.com/in/daniel-connor-wilson-a67706177/"> Linkedin </Link>                
             </div>
             <div className="flex flex-col gap-5">
-                <Link href="/"> DanielWilson.io </Link>
+                <Link href="https://danielwilson.io" target="_blank"> DanielWilson.io </Link>
             </div>
 
         </footer>
--- components/post.js
+++ components/post.js
@@ -5,21 +5,26 @@ import Image from "next/image"
 
 const Post = (data) => {
 
-    const { author, brief, thumbnail, date, topic, slug, _id } = data.data
-    console.log(data.data)
+    const { author, brief, thumbnail, date, topic, slug, _id, alt } = data.data
 
     return (
         // link around the entire card
         <Link key={_id} href={`/blog/${slug}`} className="rounded-xl overflow-hidden shadow-lg">
             <Image src={thumbnail} width={1000} height={1000} className="h-48 max-h-48" style={{ objectFit: "cover"}}/>
-            <div className="p-6 text-black">
-                <span className="text-xl font-bold"> {topic} </span>
-                <p> {brief} </p>
+            <div className="p-6 text-black flex flex-col justify-between">
+                <div>
+                    <span className="text-xl font-bold"> {topic} </span>
+                    <p> {brief} </p>                    
+                </div>
+
                 
-                <small className="flex flex-row justify-between">
-                    <span>By: {author}</span>
-                    <span> {date} </span>
-                </small>
+                <div className="h-full">
+                    <small className="flex flex-row justify-between">
+                        <span>By: {author}</span>
+                        <span> {date} </span>
+                    </small>                    
+                </div>
+
             </div>
 
         </Link>
